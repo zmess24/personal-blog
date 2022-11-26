3d155eb55d24bcdf56abfad959a054762ad32e95
Zac Messinger <zdmessinger@gmail.com>
2022-11-26 13:15:20 -0500

    started header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import './index.css';
+
+const Header: React.FC = () => {
+	return (
+		<header>
+			<h1>Zac Messinger</h1>
+			<aside id="bio-description">
+				<img
+					src="https://via.placeholder.com/150"
+					alt="Zac Messinger"
+				/>
+				<p>
+					A personal website by Zac Messinger
+					<br />I write about business & code.
+				</p>
+			</aside>
+		</header>
+	);
+};
+
+export default Header;
